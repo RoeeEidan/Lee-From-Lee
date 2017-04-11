e0c523cf2a787fe9c14a150887813d8082a27d9d
Roee Eidan <Roeeeidan@gmail.com>
2017-04-11 01:38:00 -0400

    checking

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose.connect('mongodb://localhost/data/db/');
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -11,12 +14,19 @@ db.once('open', function () {
 	console.log("Connected to db at /data/db/")
 });
 
+const PORT = process.env.PORT || 3032;
 
+
+app.use(express.static(__dirname + '/build'));
 
 app.get('/', function (req, res) { //get all review by id
     res.send("Hello World")
 })
 
-app.listen(8182, () => {
-	console.log('SERVER RUNNING ON 8182');
+app.listen(PORT , function () {
+  console.log('Example app listening on port '+PORT)
 })
+
+
+
+
