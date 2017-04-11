1ef4a3014eb066a9c5bccbd959ce5c52ab800856
Roee Eidan <Roeeeidan@gmail.com>
2017-04-11 15:25:41 -0400

    com

--- back-end/server.js
+++ back-end/server.js
@@ -6,21 +6,20 @@ const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/data/db/');
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-	console.log("Connected to db at /data/db/")
-});
+// mongoose.connect('mongodb://localhost/data/db/');
+// mongoose.Promise = global.Promise;
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+// 	console.log("Connected to db at /data/db/")
+// });
 
 const PORT = process.env.PORT || 3032;
 
-
-app.use(express.static(__dirname + '/build'));
+// app.use(express.static(__dirname + '/build'));
 
 app.get('/', function (req, res) { //get all review by id
-    res.send("Hello World")
+    res.send("<h1> Hello World </h1>")
 })
 
 app.listen(PORT , function () {
